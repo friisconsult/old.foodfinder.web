eff0717d5bd2bfd5660c00403b7aabaa3911e65e
MacFriis <per.friis@friisconsult.com>
2017-05-15 15:19:42 +0200

    Metadata

---
+++ FoodFinder.Web/Authentication/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace FoodFinder.Web.Authentication
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static Guid GetVenueId(this IPrincipal principal)
+        {
+            Guid venueId;
+            Guid.TryParse(GetClaimValueOrDefault(principal, "venueid"), out venueId);
+
+            return venueId;
+        }
+
+        public static string GetName(this IPrincipal principal)
+        {
+            return GetClaimValueOrDefault(principal, "name");
+        }
+
+        public static string GetNickName(this IPrincipal principal)
+        {
+            return GetClaimValueOrDefault(principal, "nickname");
+        }
+
+        public static string GetUserId(this IPrincipal principal)
+        {
+            return GetClaimValueOrDefault(principal, "user_id");
+        }
+
+
+        private static string GetClaimValueOrDefault(IPrincipal principal, string claimType)
+        {
+            if (claimType == null)
+                throw new ArgumentNullException(nameof(claimType));
+
+            var claimsPrincipal = principal as ClaimsPrincipal;
+
+            return claimsPrincipal?.Claims
+                .Where(x => claimType.Equals(x.Type, StringComparison.OrdinalIgnoreCase))
+                .Select(x => x.Value)
+                .FirstOrDefault();
+        }
+    }
+}
